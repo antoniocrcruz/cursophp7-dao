147e6a78730e530b4c7dd8fedcdeb5776a5d9af6
Antonio Cruz <fmcacrc2011@gmail.com>
2020-07-26 15:47:02 -0300

    Iniciamos o propjeto

---
+++ class/sql.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+class Sql extends PDO {
+
+	private $conn;
+
+	public function __construct() {
+
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root","");
+
+	}
+
+	private function setParams($statment, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($key, $value);
+		}	
+
+	}
+
+	private function setParam($statment, $key, $value){
+
+		$statement->bindParam($key, $value);
+	}
+
+	public function query($rawQuery, $params = array()){
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();//neste ponto, você não está esperando dados de volta, apenas determinando que ele execute a query, se tivesse um select, aí sim precisaria
+
+		return $stmt;//neste ponto, você não está esperando dados de volta, apenas determinando que ele execute a query, se tivesse um select, aí sim precisaria
+		
+		}
+
+		public function select($rawQuery, $params = array()): array
+		{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}	
+
+}
+
+ ?>
